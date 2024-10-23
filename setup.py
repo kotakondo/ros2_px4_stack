6d769daf419a9e01cb233d4fe7772958bd646a5c
Juan Rached <juanrached@gmail.com>
2024-10-23 17:13:51 -0400

    Add Launch File to setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', ['launch/mocap_offboard.launch']),
         ('share/' + package_name + '/launch', ['launch/mocap_offboard.launch.py']),
+        ('share/' + package_name + '/launch', ['launch/offboard_square_example.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
