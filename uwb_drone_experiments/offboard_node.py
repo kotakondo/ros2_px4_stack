7692f9b13bdedb7a3632b14c1a4133e63edac8b8
Juan Rached <juanrached@gmail.com>
2024-10-25 16:40:00 -0400

    Debug Offboard Message

--- uwb_drone_experiments/offboard_node.py
+++ uwb_drone_experiments/offboard_node.py
@@ -110,7 +110,7 @@ class OffboardPathFollower(BasicMavrosInterface):
         rate2 = self.create_rate(1/0.2)
         # wait 1 second for FCU connection
         # rclpy.sleep(1)
-        # self.wait_for_seconds(1)
+        self.wait_for_seconds(1)
         # rate1.sleep()
 
         self.get_logger().info("HERE 5")
