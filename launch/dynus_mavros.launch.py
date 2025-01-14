6ed68bd799d733e16b3c3e30ddab4de572bfb40c
Juan Rached <juanrached@gmail.com>
2025-01-14 12:12:25 -0500

    Fix Frames

--- launch/dynus_mavros.launch.py
+++ launch/dynus_mavros.launch.py
@@ -19,6 +19,8 @@ def generate_launch_description():
         DeclareLaunchArgument('ns', default_value=EnvironmentVariable("VEH_NAME")),
         DeclareLaunchArgument('fcu_url', default_value='/dev/ttyACM0:921600'),
         DeclareLaunchArgument('respawn_mavros', default_value='false'),
+        
+        # Run dynus node
         Node(
             package='ros2_px4_stack',
             executable='track_dynus_traj',
@@ -26,32 +28,34 @@ def generate_launch_description():
             namespace=namespace,
             output='screen',
         ),
-        # Node(
-        #     package='tf2_ros',
-        #     executable='static_transform_publisher',
-        #     name='odom_to_mavros',
-        #     arguments=['0', '0', '0', '-1.57', '3.14', '0', 'odom', 'world_mavros']
-        # ),
+
+        # Static transforms
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='odom_to_mocap',
             arguments=[init_x, init_y, init_z, init_yaw, init_pitch, init_roll, 'world_mocap', f'{veh}/init_pose'] 
         ),
-        # Node(
-        #     package='tf2_ros',
-        #     executable='static_transform_publisher',
-        #     name='map_to_odom',
-        #     arguments=['0', '0', '0', '0', '0', '0', 'odom', 'map']
-        # ),
-        # Node(
-        #     package='tf2_ros',
-        #     executable='static_transform_publisher',
-        #     name='base_link_to_ns',
-        #     arguments=['0', '0', '0', '0', '0', '0', f"/{veh}/base_link", '/base_link'] 
-        # ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='map_to_odom',
+            arguments=['0', '0', '0', '0', '0', '0', 'world', 'world_mocap']
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='map_to_odom',
+            arguments=['0', '0', '0', '0', '0', '0', 'world', 'map']
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='base_to_d455',
+            arguments=['0', '0', '0', '0', '0', '0', f"{veh}/base_link", f"{veh}/d455_link"]
+        ),
 
-        # Launch the repub_mocap node
+        # Run repub_livox node
         Node(
             package='ros2_px4_stack',
             executable='repub_livox',
