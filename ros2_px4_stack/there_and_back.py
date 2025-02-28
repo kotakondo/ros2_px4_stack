3ea6a5438af8cd9cc9ff91a6785d71b4a66f3ab0
Juan Rached <juanrached@gmail.com>
2025-02-28 13:08:14 -0500

    Get Initial Position From Static Transform

--- ros2_px4_stack/there_and_back.py
+++ ros2_px4_stack/there_and_back.py
@@ -6,6 +6,7 @@ import rclpy
 from rclpy.node import Node 
 from geometry_msgs.msg import PoseStamped 
 from rclpy.qos import QoSProfile, DurabilityPolicy, ReliabilityPolicy 
+from tf2_ros import Buffer, TransformListener, TransformBroadcaster
 
 class ThereAndBackAgain(Node):
     def __init__(self, x, y, z):
@@ -13,9 +14,6 @@ class ThereAndBackAgain(Node):
 
         # get namespace and initial position
         self.veh = os.environ.get("VEH_NAME")
-        self.x_init = float(os.environ.get("INIT_X"))
-        self.y_init = float(os.environ.get("INIT_Y"))
-        self.z_init = float(os.environ.get("INIT_Z")) 
 
         # initialize term_goal 
         self.x_term_goal = x
@@ -23,7 +21,7 @@ class ThereAndBackAgain(Node):
         self.z_term_goal = z 
 
         # initialize goal (takeoff)
-        self.goal = (self.x_init, self.y_init, self.z_term_goal)
+        self.goal = None 
         
         # create qos policy 
         qos_profile1 = QoSProfile(depth=10)
@@ -47,49 +45,67 @@ class ThereAndBackAgain(Node):
         # agent frame term_goal 
         self.goal_af = None 
 
+        # get init_pose transform 
+        self.init_pose_tf = None
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+        timer_period = 1 / 10
+        self.timer = self.create_timer(timer_period, self.get_transform)
+
+
+
+    def get_transform(self):
+        # Get initial pose from static transform 
+        if not self.init_pose_tf:
+            while not self.tf_buffer.lookup_transform("world_mocap", f"{self.veh}/init_pose", self.get_clock().now()):
+                self.get_logger().info("Waiting on init_pose transform")
+            
+            self.init_pose_tf = self.tf_buffer.lookup_transform("world_mocap", f"{self.veh}/init_pose", self.get_clock().now())
+            translation = self.init_pose_tf.transform.translation
+            self.goal = (translation.x, translation.y, self.z_term_goal)
 
     def goal_af_cb(self, msg):
         self.goal_af = (msg.pose.position.x, msg.pose.position.y, msg.pose.position.z)  
 
-    
     def state_cb(self, msg: PoseStamped):
-        # Extract state from message 
-        state = (msg.pose.position.x, msg.pose.position.y, msg.pose.position.z)
+        if self.goal: 
+            # Extract state from message 
+            state = (msg.pose.position.x, msg.pose.position.y, msg.pose.position.z)
 
-        if self.goal_af == None: 
-            new_goal = PoseStamped() 
-            new_goal.header.stamp = msg.header.stamp
-            new_goal.header.frame_id = f"/{self.veh}/term_goal"
-            new_goal.pose.position.x = self.goal[0]
-            new_goal.pose.position.y = self.goal[1]
-            new_goal.pose.position.z = self.goal[2]
-            new_goal.pose.orientation.x = 0.0
-            new_goal.pose.orientation.y = 0.0
-            new_goal.pose.orientation.z = 0.0
-            new_goal.pose.orientation.w = 1.0
+            if self.goal_af == None: 
+                new_goal = PoseStamped() 
+                new_goal.header.stamp = msg.header.stamp
+                new_goal.header.frame_id = f"/{self.veh}/term_goal"
+                new_goal.pose.position.x = self.goal[0]
+                new_goal.pose.position.y = self.goal[1]
+                new_goal.pose.position.z = self.goal[2]
+                new_goal.pose.orientation.x = 0.0
+                new_goal.pose.orientation.y = 0.0
+                new_goal.pose.orientation.z = 0.0
+                new_goal.pose.orientation.w = 1.0
 
-            self.goal_pub.publish(new_goal)
-            
-        if self.goal_af and self.reached_goal(state):
-            # Change goal 
-            if self.goal == (self.x_init, self.y_init, self.z_term_goal):
-                self.goal = (self.x_term_goal, self.y_term_goal, self.z_term_goal)
-            elif self.goal == (self.x_term_goal, self.y_term_goal, self.z_term_goal):
-                self.goal = (self.x_init, self.y_init, self.z_term_goal)
+                self.goal_pub.publish(new_goal)
+                
+            if self.goal_af and self.reached_goal(state):
+                # Change goal 
+                if self.goal == (self.x_init, self.y_init, self.z_term_goal):
+                    self.goal = (self.x_term_goal, self.y_term_goal, self.z_term_goal)
+                elif self.goal == (self.x_term_goal, self.y_term_goal, self.z_term_goal):
+                    self.goal = (self.x_init, self.y_init, self.z_term_goal)
 
-            # Publish new goal in global frame 
-            new_goal = PoseStamped() 
-            new_goal.header.stamp = msg.header.stamp
-            new_goal.header.frame_id = f"/{self.veh}/term_goal"
-            new_goal.pose.position.x = self.goal[0]
-            new_goal.pose.position.y = self.goal[1]
-            new_goal.pose.position.z = self.goal[2]
-            new_goal.pose.orientation.x = 0.0
-            new_goal.pose.orientation.y = 0.0
-            new_goal.pose.orientation.z = 0.0
-            new_goal.pose.orientation.w = 1.0
+                # Publish new goal in global frame 
+                new_goal = PoseStamped() 
+                new_goal.header.stamp = msg.header.stamp
+                new_goal.header.frame_id = f"/{self.veh}/term_goal"
+                new_goal.pose.position.x = self.goal[0]
+                new_goal.pose.position.y = self.goal[1]
+                new_goal.pose.position.z = self.goal[2]
+                new_goal.pose.orientation.x = 0.0
+                new_goal.pose.orientation.y = 0.0
+                new_goal.pose.orientation.z = 0.0
+                new_goal.pose.orientation.w = 1.0
 
-            self.goal_pub.publish(new_goal)
+                self.goal_pub.publish(new_goal)
                 
     def reached_goal(self, state: tuple, tol: float=0.25):
         # Compare state and goal in the agent frame 
