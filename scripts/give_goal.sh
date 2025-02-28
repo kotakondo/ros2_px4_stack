a898b543a5f0d5ee80f881a0e1c06a08bf299679
Juan Rached <juanrached@gmail.com>
2025-02-28 14:46:28 -0500

    Minor

--- scripts/give_goal.sh
+++ scripts/give_goal.sh
@@ -4,4 +4,4 @@ pos_x=$1
 pos_y=$2
 pos_z=$3
 
-ros2 topic pub /PX01/term_goal geometry_msgs/msg/PoseStamped "{header: {stamp: {sec: 0.0, nanosec: 0.0}, frame_id: map}, pose: {position: {x: ${pos_x}, y: ${pos_y}, z: ${pos_z}}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}}}" -1 
+ros2 topic pub /${VEH_NAME}/term_goal geometry_msgs/msg/PoseStamped "{header: {stamp: {sec: 0.0, nanosec: 0.0}, frame_id: map}, pose: {position: {x: ${pos_x}, y: ${pos_y}, z: ${pos_z}}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}}}" -1 
