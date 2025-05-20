3954c86d852fb97119f1466d45aac294d56519ab
Juan Rached <juanrached@gmail.com>
2025-05-20 19:51:42 -0400

    Minor

--- setup.py
+++ setup.py
@@ -23,13 +23,11 @@ setup(
     entry_points={
         'console_scripts': [
             'base_mavros_interface = ros2_px4_stack.src.base_mavros_interface:main',
-            'repub_mocap = ros2_px4_stack.transforms.repub_mocap:main',
             'repub_odom = ros2_px4_stack.transforms.repub_odom:main',
             'track_dynus_traj = ros2_px4_stack.src.track_dynus_traj:main',
             'track_gen_traj = ros2_px4_stack.src.track_gen_traj:main',
             'trajgen_offboard_node = ros2_px4_stack.src.trajgen_offboard_node:main',
             'dynus_offboard_node = ros2_px4_stack.src.dynus_offboard_node:main',
-            'repub_livox = ros2_px4_stack.transforms.repub_livox:main',
             'get_init_pose = ros2_px4_stack.transforms.get_init_pose:main',
             'mocap_to_livox_frame = ros2_px4_stack.transforms.mocap_to_livox_frame:main',
             'there_and_back = ros2_px4_stack.trajectories.there_and_back:main',
